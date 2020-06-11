ae46692fefe06e0338d609231658add3e51c3448
dmitry-bl1nd <bogatyrov200488@gmail.com>
2020-06-11 10:34:30 +0500

    Верстка header

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Building(models.Model):
     type = models.CharField("Категория", max_length=100)
     price = models.PositiveIntegerField("Цена")
-
+    img = models.CharField("Картинка", max_length=250, default="1.jpeg")
     class Meta:
         verbose_name = 'Объявление'
         verbose_name_plural = 'Объявления'
