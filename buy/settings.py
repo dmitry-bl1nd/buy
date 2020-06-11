844e079acfaff4913ceca04eabd1c4f03e77cc40
dmitry-bl1nd <bogatyrov200488@gmail.com>
2020-06-11 21:17:01 +0500

    Вывод изображений

--- buy/settings.py
+++ buy/settings.py
@@ -121,4 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "main/static/")
+STATIC_ROOT = '/var/www/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "media"),
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, "media")
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,14 @@
+from django.conf import settings
 from django.urls import path
+from django.contrib.staticfiles.urls import static
 from . import views
 
 urlpatterns = [
 	path('', views.index, name = 'index'),
 	path('test', views.test, name = 'test'),
 ]
+
+if settings.DEBUG:
+	urlpatterns += static (settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
